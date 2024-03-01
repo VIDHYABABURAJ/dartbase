797383c1522030bda7dda51eedafb602b39843a1
VIDHYABABURAJ <vidhyababurajvidhya@gmail.com>
2024-03-01 19:29:24 +0530

    task

--- bin/dartbase.dart
+++ bin/dartbase.dart
@@ -1,21 +1,24 @@
 void main() {
+String sl="1";
+  String n="Riya";
+  String s="Maths";
+  int  m=98;
 
-  const int a =10;
+  String s_l="2";
+  String n_a="Diya";
+  String s_u="Maths";
+  int  m_a=95;
 
-  final int  b;
-  b=34;
+  print(sl);
+  print(n);
+  print(s);
+  print(m);
+  print(s_l);
+  print(n_a);
+  print(s_u);
+  print(m_a);
 
-  late int g;
-  g = 90;
-  print(g);
-  g=89;
-  print(g);
-
-  print(a);
-  print(b);
-
-
-
-
-
+  int x=int.parse(sl);
+  print(x);
+  print(x.runtimeType);
 }
